16c00dc7e751779cfef24d12a798c6f978fd2d33
Jeffrey-F-Guo <jeffrey.guo00703@gmail.com>
2025-12-22 00:25:16 -0800

    File upload trigger works

--- Frontend/src/pages/Home/Home.tsx
+++ Frontend/src/pages/Home/Home.tsx
@@ -1,14 +1,15 @@
 import './Home.css'
 import FileUpload from '../../components/FileUpload/FileUpload.tsx'
-
 function app() {
 
     return (
         <div>
             <h1>Lamp</h1>
+
             <div className='file-upload-container'>
                 <FileUpload />
             </div>
+            
             
 
         </div>
