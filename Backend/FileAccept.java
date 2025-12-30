730d8951c04c0cbe32baa07487bf36291557ddbc
Jeffrey-F-Guo <jeffrey.guo00703@gmail.com>
2025-12-30 12:33:44 -0800

    Escaping the python spammer allegations

---
+++ Backend/FileAccept.java
@@ -0,0 +1,11 @@
+package Backend;
+
+public class FileAccept {
+    String UPLOAD_DIR_NAME = "uploads/";
+    int MAX_FILE_SIZE = 10 * 1024 * 1024;
+    
+    public static void main(String[] args) {
+        System.out.println("Hellow");
+
+    }
+}
